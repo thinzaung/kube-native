9626ca66e3cdb831b16834bfc97ed41d24bd1e3b
thinzaung <thinzaung@gmail.com>
2021-07-27 22:53:02 +0000

    end of exercise checkpoint

--- bulletin-board-app/server.js
+++ bulletin-board-app/server.js
@@ -5,11 +5,24 @@ var express        = require('express'),
     morgan         = require('morgan'),
     routes         = require('./backend'),
     api            = require('./backend/api');
+    prometheus     = require('prom-client');
 
 var app = module.exports = express();
 
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
+
+// middleware monitoring function
+var monitor = function (req, res, next){
+  res.on("finish", function() {
+      routeRequests.labels(req.method, req.path, res.statusCode).inc()
+  });
+
+  next();
+}
+// middleware stack
+app.use(monitor)
+
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -29,10 +42,23 @@ if ('production' == app.get('env')) {
   app.use(errorHandler());
 }
 
+// counter for Prometheus:
+const routeRequests = new prometheus.Counter({
+  name: 'bb_route_requests',
+  help: 'request by route', 
+  labelNames: ['method', 'route', 'code']
+})
+
 app.get('/', routes.index);
 app.get('/api/events', api.events);
 app.post('/api/events', api.event);
 app.delete('/api/events/:eventId', api.event);
+app.get('/metrics', (req, res) => {
+  res.set('Content-Type', prometheus.register.contentType)
+  res.end(prometheus.register.metrics())
+})
+
+prometheus.collectDefaultMetrics();
 
 app.listen(8080);
 console.log('Magic happens on port 8080...');
